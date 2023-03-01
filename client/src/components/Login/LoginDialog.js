3388e0df664341aa4ec60cdd56ffe6ac37d4c8a8
Yethu <Yethukichu55@gmail.com>
2023-03-01 20:28:32 +0530

    dialog contents are modified

--- client/src/components/Login/LoginDialog.js
+++ client/src/components/Login/LoginDialog.js
@@ -3,15 +3,52 @@ import { Box } from '@mui/system'
 import React from 'react'
 
 const Component=styled(Box)`
-height: 70vh;
-width:90vh;
+height: 60vh;
+width:70vh;
+`;
+const Content=styled(Box)`
+display: flex;
+flex-direction: column;
+width: 60%;
+padding: 50px 35px;
 `;
 
 const Image=styled(Box)
-`background: #2874f0 url("https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png") no-repeat center center;
+`background: #2874f0 url("https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png") no-repeat center 85%;
  height: 100%;
- width:30%;`
+ width:40%;`;
 
+ const Wrapper=styled(Box)`
+ display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 50px 15px;
+
+ `;
+const LoginButton=styled(Button)`
+background-color: #fb641b;
+color: #fff;
+text-transform: none;
+height: 48px;
+border-radius: 2px;
+margin-top: 20px;
+width: 65%;
+margin-left: 26px;
+
+margin-bottom: 10px;
+`;
+const RequestButton=styled(Button)`
+background-color: #fff;
+color: #2874f0;
+box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+text-transform: none;
+height: 48px;
+border-radius: 2px;
+margin-top: 20px;
+width: 65%;
+margin-left: 26px;
+
+margin-bottom: 10px;`;
 
 const LoginDialog = ({open,setopen}) => {
   const handleClose = () => {
@@ -22,17 +59,22 @@ const LoginDialog = ({open,setopen}) => {
       <Dialog open={open} onClose={handleClose} >
         <Component>
           <Box sx={{display:"flex",height:"100%"}}>
-              <Image></Image>
-              <Box sx={{width:"90vw"}}>
+              <Image>
+                <Wrapper>
+                  <Typography variant="h4" sx={{color:"black"}}>Login</Typography>
+                  <Typography variant="h7" sx={{color:"black"}}>Get access to your Orders, Wishlist and Recommendations</Typography>
+                </Wrapper>
+              </Image>
+              <Content>
                 <TextField label="Enter Email/Mobile Number" variant="standard" />
                 <TextField label="Enter Password" variant="standard" />
                 <Typography>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</Typography>
-                <Button>Login</Button>
+                <LoginButton>Login</LoginButton>
                 <Typography>OR</Typography>
-                <Button>Request OTP</Button>
+                <RequestButton>Request OTP</RequestButton>
                 <Typography>New to Flipkart? Create an account</Typography>
 
-              </Box>
+              </Content>
             </Box>
         </Component>
 
